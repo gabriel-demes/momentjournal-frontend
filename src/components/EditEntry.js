0598bc58e1b8f27ed0f2773746eb564d38179331
gabriel-demes <Gabrieldemes@nyu.edu>
2021-04-08 15:32:33 -0400

    speech to text feature complete

--- src/components/EditEntry.js
+++ src/components/EditEntry.js
@@ -1,11 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect} from "react";
 import "../css/EditEntry.css";
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
+
 
 const EditEntry = ({ handlePopUp, title, body, id, setTitle, setBody }) => {
     const [myTitle, setMyTitle] = useState(title);
     const [myBody, setMyBody] = useState(body);
+    
+    const commands = [
+        {
+            command: "journal stop listening",
+            callback: ()=> handleStop()
+        },
+        {
+            command: "journal title is *",
+            callback: (title) => setMyTitle(title)
+        },
+        {
+            command: "journal save entry",
+            callback: () => handleSave()
+        }
+    ]
+    const { transcript, resetTranscript , interimTranscript, finalTranscript} = useSpeechRecognition({commands})
 
     const handleSave = () => {
+        SpeechRecognition.stopListening()
+        resetTranscript()
         fetch(`http://localhost:3000/entries/${id}`, {
         method: "PATCH",
         headers: { "Content-Type": "application/json" },
@@ -14,13 +34,29 @@ const EditEntry = ({ handlePopUp, title, body, id, setTitle, setBody }) => {
         .then((r) => r.json())
         .then((entry) => {
             setTitle(entry.title);
-            setBody(entry.body);
+            setBody(entry.body)
+            handlePopUp();
         });
     };
+
+    useEffect(()=>{
+        setMyBody(myBody=> (myBody + " " + finalTranscript).replace(/Journal stop listening| Journal save entry| Journal title is [a-z0-9\s]*/gi,''))
+        resetTranscript()
+    }, [finalTranscript])
 
     const handleCancel = () => {
         handlePopUp();
     };
+
+    const handleStart = () => {
+        SpeechRecognition.startListening({continuous: true})
+        
+    }
+    const handleStop = () => {
+        SpeechRecognition.stopListening()
+        resetTranscript()
+    }
+    
 
     return (
         <div className="hide">
@@ -36,6 +72,8 @@ const EditEntry = ({ handlePopUp, title, body, id, setTitle, setBody }) => {
         </div>
         <button onClick={handleSave}>Save</button>
         <button onClick={handleCancel}>cancel</button>
+        <button onClick={handleStart}>Start</button>
+        <button onClick={handleStop}>Stop</button>
         </div>
     );
 };
