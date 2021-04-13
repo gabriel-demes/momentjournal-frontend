7960163b6b0108f922fb7e2e4612fc9ac14a0ff5
gabriel-demes <Gabrieldemes@nyu.edu>
2021-04-12 20:34:39 -0400

    navbar setup complete

--- src/components/App.js
+++ src/components/App.js
@@ -29,7 +29,7 @@ function App() {
 
   return (
     <>
-      <NavBar/>
+      <NavBar user={user}/>
       <Switch>
         {user ? 
           <>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,24 @@
 import React from "react"
+import "../css/Navbar.css";
+import {useHistory} from "react-router-dom"
 
-const NavBar = () => {
+const NavBar = ({user}) => {
+
+    const history = useHistory()
+    const changeTabs = (e) => {
+        history.push(`/${e.target.id}`)
+    }
     return(
-        <div>
-            <h3>NavBar</h3>
+        <div className="navbar">
+            <button>ADD</button>
+            <div className="tabs">
+            <div onClick={changeTabs} id="me">DashBoard</div>
+            <div onClick={changeTabs} id="mygoals">My Goals</div>
+            <div>Resources</div>
+            </div>
+            <h3>Welcome {user? user.name:null}</h3>
+            <div>User Avatar</div>
+
         </div>
     )
 }
